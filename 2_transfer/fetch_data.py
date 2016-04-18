2d9bcb36221c677aaf58e265fc905a7514da96ee
Tomas Reimers <tomasreimers@gmail.com>
2016-04-18 11:21:19 -0400

    Added train/test spilt

--- 2_transfer/fetch_data.py
+++ 2_transfer/fetch_data.py
@@ -30,7 +30,7 @@ def get_images(tags, total):
             to_return.append(url)
     return to_return
 
-for tag, filetag, filenum in [("flower", "flower", 100), ("-flower", "notflower", 200)]:
+for tag, filetag, filenum in [("car", "car", 500), ("-car", "notcar", 1000)]:
     # create dir
     cur_output_dir = os.path.join(output_file_dir, filetag)
     if not os.path.exists(cur_output_dir):
