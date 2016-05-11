42fe7bb021d267b6d49bbaf966719f28d1548588
Joseph Kahn <josephkahn@college.harvard.edu>
2016-05-11 15:40:51 -0400

    cleanup

--- 4_lesion/README.md
+++
@@ -1,1 +0,0 @@
-How can we make testing more flexible? Ideally we'd load up an old model into a testing module, or something along those lines.
--- final/lib/log.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def log(*args):
-    print(' '.join([str(arg) for arg in args]))
-
-def log_ephemeral(string):
-    sys.stdout.write(string + '\r')
-    sys.stdout.flush()
