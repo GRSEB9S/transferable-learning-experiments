06f1c5cbc2dd4d7a20dd6303d60f14c03c7140f3
Joseph Kahn <josephkahn@college.harvard.edu>
2016-05-11 15:41:51 -0400

    cleanup

--- 4_lesion/utils.py
+++
@@ -1,7 +0,0 @@
-import datetime
-import numpy as np
-
-def date_stamp():
-    return str(datetime.datetime.now()).split('.')[0][8:].replace(' ', '_').replace(':', '-')
-
-directories = ['bunny_2800', 'eye_2800', 'dolphin_2800', 'dog_2800']
